c14fea560b86e8c46bfea2c7cbbc9689841288da
TonhaoSemAcento <tonhaosemacento@gmail.com>
2019-01-24 00:58:03 -0200

    Add project files.

---
+++ ShellPreview2/ShellPreview2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ShellPreview2.Models;
+
+namespace ShellPreview2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
